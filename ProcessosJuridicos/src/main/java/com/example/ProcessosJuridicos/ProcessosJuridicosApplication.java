58cd1d4ac9c9ee1b082ad4195629ad6316dd9cce
DennerOl <dener155@hotmail.com>
2024-08-20 20:14:19 -0300

    Projeto criado

---
+++ ProcessosJuridicos/src/main/java/com/example/ProcessosJuridicos/ProcessosJuridicosApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProcessosJuridicos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessosJuridicosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessosJuridicosApplication.class, args);
+	}
+
+}
---
+++ ProcessosJuridicos/src/test/java/com/example/ProcessosJuridicos/ProcessosJuridicosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProcessosJuridicos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessosJuridicosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
